eae624129b717f6288d9d0c6e2d3042e5a1174cb
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-05-09 12:16:22 +0100

    works

--- logic.c
+++ logic.c
@@ -14,13 +14,14 @@ typedef struct {
 
 void showMenu() {
     printf("===== Menu =====\n");
-    printf("1. Perform Action 1\n");
+    printf("1. Add Student\n");
     printf("2. Perform Action 2\n");
-    printf("3. Add Student\n");
+    printf("3. Perform Action 3\n");
+    printf("4. Exit\n"); 
     printf("================\n");
 }
 
-int isIdExists(int id, const char *filename) {
+int studentExist(int id, const char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening file.\n");
@@ -42,27 +43,27 @@ int isIdExists(int id, const char *filename) {
     return found;
 }
 
-void addStudent(const char *filename) {
+int addStudent(const char *filename) {
     FILE *file = fopen(filename, "a");
     if (file == NULL) {
         printf("Error opening file.\n");
-        return;
+        return 0;
     }
 
     Student student;
     printf("Enter student ID: ");
     scanf("%d", &student.id);
 
-    if (isIdExists(student.id, filename)) {
+    if (studentExist(student.id, filename)) {
         printf("A student with the same ID already exists.\n");
         fclose(file);
-        return;
+        return 0;
     }
 
     printf("Enter your full name: ");
     scanf(" %[^\n]", student.name);
 
-    printf("Enter student birth date (yyyy-mm-dd): "); // Modified prompt
+    printf("Enter student birth date (yyyy-mm-dd): ");
     scanf(" %[^\n]", student.birthDate);
 
     printf("Enter student school (SBA/SHSS/SSE): ");
@@ -84,24 +85,31 @@ void addStudent(const char *filename) {
 
     fclose(file);
     printf("Student added successfully.\n");
+    return 1; // Return success status
 }
 
 void performAction(int choice, const char *filename) {
     switch (choice) {
         case 1:
-            printf("Action 1 selected.\n");
-            // Implement the code for action 1 here
+            printf("Add a new student, if student ID already exists, try modifying.\n");
+            while (1) {
+                int added = addStudent(filename);
+                if (added)
+                    break;
+            }
             break;
         case 2:
             printf("Action 2 selected.\n");
+            // Implement the code for action 1 here
+            break;
+        case 33:
+            printf("Action 3 selected.\n");
             // Implement the code for action 2 here
             break;
-        case 3:
-            printf("Add a new student, if student ID already exists, try modifying.\n");
-            addStudent(filename);
-            break;
+        case 4:
+            printf("Exiting the program.\n");
+            exit(0); // Exit the program with a status of 0
         default:
             printf("Invalid choice.\n");
     }
 }
-
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,31 @@
-username
+#include "functions.c"
+#include "logic.c" 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_USERNAME_LENGTH 50
+#define MAX_PASSWORD_LENGTH 50
+#define USERS "users.txt"
+#define STUDENTS "students.txt"
+#define MAX_ATTEMPTS 3
+
+int main() {
+    User user;
+    int success = 0;
+
+    success = attemptLogin(MAX_ATTEMPTS, &user, USERS);
+
+    if (success) {
+        printf("Welcome, %s!\n", user.username);
+        int choice;
+        showMenu();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        performAction(choice, STUDENTS);
+    } else {
+        printf("Max attempts reached. Access denied.\n");
+    }
+
+    return 0;
+}
