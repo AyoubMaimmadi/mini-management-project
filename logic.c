b9d621f7927e462874c062bf2812534f5bbda346
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-05-09 12:39:36 +0100

    works

--- logic.c
+++ logic.c
@@ -15,7 +15,7 @@ typedef struct {
 void showMenu() {
     printf("===== Menu =====\n");
     printf("1. Add Student\n");
-    printf("2. Perform Action 2\n");
+    printf("2. Modify Student Information\n");
     printf("3. Perform Action 3\n");
     printf("4. Exit\n"); 
     printf("================\n");
@@ -88,6 +88,75 @@ int addStudent(const char *filename) {
     return 1; // Return success status
 }
 
+
+////////////////////////////////////// Modify Student Information //////////////////////////////////////
+
+int modifyStudent(const char *filename) {
+    FILE *file = fopen(filename, "r+");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 0;
+    }
+
+    int studentId;
+    printf("Enter student ID to modify: ");
+    scanf("%d", &studentId);
+
+    if (!studentExist(studentId, filename)) {
+        printf("Student with ID %d does not exist.\n", studentId);
+        fclose(file);
+        return 0;
+    }
+
+    // Find the position of the student in the file
+    long pos = -1;
+    char line[100];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        int currentId = 0;
+        sscanf(line, "%d,", &currentId);
+        if (currentId == studentId) {
+            pos = ftell(file) - strlen(line);
+            break;
+        }
+    }
+
+    if (pos == -1) {
+        printf("Error finding student position in the file.\n");
+        fclose(file);
+        return 0;
+    }
+
+    Student student;
+    printf("Enter modified full name: ");
+    scanf(" %[^\n]", student.name);
+
+    printf("Enter modified birth date (yyyy-mm-dd): ");
+    scanf(" %[^\n]", student.birthDate);
+
+    printf("Enter modified school (SBA/SHSS/SSE): ");
+    scanf(" %[^\n]", student.school);
+
+    printf("Enter modified major: ");
+    scanf(" %[^\n]", student.major);
+
+    printf("Enter modified credits (0-140): ");
+    scanf("%d", &student.credits);
+
+    printf("Enter modified CGPA (0-4): ");
+    scanf("%f", &student.cgpa);
+
+    printf("Enter modified entry semester (e.g., Fall 2021): ");
+    scanf(" %[^\n]", student.entrySemester);
+
+    fseek(file, pos, SEEK_SET);
+    fprintf(file, "%d,%s,%s,%s,%s,%d,%.2f,%s\n", studentId, student.name, student.birthDate, student.school, student.major, student.credits, student.cgpa, student.entrySemester);
+
+    fclose(file);
+    printf("Student with ID %d modified successfully.\n", studentId);
+    return 1; 
+}
+
+//////////////////////////////////////////// Action ////////////////////////////////////////////
 void performAction(int choice, const char *filename) {
     switch (choice) {
         case 1:
@@ -99,12 +168,12 @@ void performAction(int choice, const char *filename) {
             }
             break;
         case 2:
-            printf("Action 2 selected.\n");
-            // Implement the code for action 1 here
+            printf("Modify Student Information by given ID.\n");
+            modifyStudent(filename);
             break;
-        case 33:
+        case 3:
             printf("Action 3 selected.\n");
-            // Implement the code for action 2 here
+            // Implement the code for action 3 here
             break;
         case 4:
             printf("Exiting the program.\n");
