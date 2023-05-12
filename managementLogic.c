3cf4de01f6dfad8fadfbb9a563f2a76d6630036f
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-05-12 10:48:21 +0100

    new

--- managementLogic.c
+++ managementLogic.c
@@ -46,7 +46,7 @@ int isValidCGPA(float cgpa) {
     return (cgpa >= 0 && cgpa <= 4);
 }
 
-// function that checks if the student exists in the file
+// function that checks if the student exists in the file or not, 1 if exists and 0 otherwise
 int studentExists(int id, const char *filename) {
     // this function returns 1 if the student exists in the file and 0 otherwise
     FILE *file = fopen(filename, "r");
@@ -55,43 +55,54 @@ int studentExists(int id, const char *filename) {
         return 0;
     }
 
+    // initialize the found variable to 0 and line array to 100 in size
     int found = 0;
     char line[100];
+    // loop through the file line by line
     while (fgets(line, sizeof(line), file) != NULL) {
+        // initialize the currentId variable to 0
         int currentId = 0;
+        // scan the line and store the id in the currentId variable
         sscanf(line, "%d,", &currentId);
+        // if the currentId is equal to the id then set found to 1 and break the loop
         if (currentId == id) {
             found = 1;
             break;
         }
     }
 
+    // close the file 
     fclose(file);
+    // return the found variable (1 if the student exists and 0 otherwise)
     return found;
 }
 
 //////////////////////////////////////// Add Student ////////////////////////////////////////
 int addStudent(const char *filename) {
+    // open the file in append mode
     FILE *file = fopen(filename, "a");
     if (file == NULL) {
         printf("Error opening file.\n");
         return 0;
     }
-
+    // initialize the student struct
     Student student;
+    // get the student id from the user
     printf("Enter student ID (must be a unique number): ");
     scanf("%d", &student.id);
-
+    
+    // Check if the student already exists
     if (studentExists(student.id, filename)) {
         printf("A student with the same ID already exists.\n");
         fclose(file);
         return 0;
     }
 
+    // get the student name from the user
     printf("Enter your full name (e.g., John Doe): ");
     getchar(); // Clear the newline character from the previous input
     fgets(student.name, sizeof(student.name), stdin);
-    student.name[strcspn(student.name, "\n")] = '\0'; // Remove the newline
+    student.name[strcspn(student.name, "\n")] = '\0'; // remove the newline because it gave me some problems
 
     printf("Enter student birth date (yyyy-mm-dd): ");
     fgets(student.birthDate, sizeof(student.birthDate), stdin);
@@ -104,16 +115,18 @@ int addStudent(const char *filename) {
         return 0;
     }
 
+    // Get the birth year from the birth date and store it in the year variable
     int year;
     sscanf(student.birthDate, "%d", &year);
 
     // Check if the birth year falls within the specified range
     if (!isValidBirthYear(year)) {
-        printf("Invalid birth year. Please enter a value between 1900 and 2023.\n");
+        printf("Invalid birth year. Please enter a value between 1995 (starting of AUI) and 2010.\n");
         fclose(file);
         return 0;
     }
 
+    // get the student information from the user and store it in the student struct
     printf("Enter student school (SBA/SHSS/SSE): ");
     scanf(" %[^\n]", student.school);
 
@@ -123,7 +136,7 @@ int addStudent(const char *filename) {
     printf("Enter student credits (0-140): ");
     scanf("%d", &student.credits);
 
-     // Check if the credits fall within the specified range
+     // Check if the credits fall within the specified range (0-140)
     if (!isValidCredit(student.credits)) {
         printf("Invalid credits. Please enter a value between 0 and 140.\n");
         fclose(file);
@@ -133,6 +146,7 @@ int addStudent(const char *filename) {
     printf("Enter student CGPA (0-4): ");
     scanf("%f", &student.cgpa);
 
+    // Check if the cgpa fall within the specified range (0-4)
     if (!isValidCGPA(student.cgpa)) {
         printf("Invalid CGPA. Please enter a value between 0 and 4.\n");
         fclose(file);
@@ -142,53 +156,68 @@ int addStudent(const char *filename) {
     printf("Enter student entry semester (e.g., Fall 2021): ");
     scanf(" %[^\n]", student.entrySemester);
 
+    // Write the student information to the file
     fprintf(file, "%d,%s,%s,%s,%s,%d,%.2f,%s\n", student.id, student.name, student.birthDate, student.school, student.major, student.credits, student.cgpa, student.entrySemester);
 
-
+    // Close the file
     fclose(file);
     printf("\n\n----> Student added successfully.\n\n");
-    return 1; // Return success status
+    return 1; // return 1 for a success status
 }
 
 ////////////////////////////////////// Modify Student Information //////////////////////////////////////
 int modifyStudent(const char *filename) {
+    // Open the file in read and write mode
     FILE *file = fopen(filename, "r+");
     if (file == NULL) {
         printf("Error opening file.\n");
         return 0;
     }
 
+    // Get the student id from the user
     int studentId;
     printf("Enter student ID to modify: ");
     scanf("%d", &studentId);
 
+    // check if the student exists
     if (!studentExists(studentId, filename)) {
         printf("Student with ID %d does not exist.\n", studentId);
         fclose(file);
         return 0;
     }
 
-    // Find the position of the student in the file
+    // find the position of the student in the file
+    // we initialize the pos variable to -1 that indicates that the student is not found (defalut)
     long pos = -1;
+    // initialize the line array to 100 in size
     char line[100];
+    // loop through the file line by line
     while (fgets(line, sizeof(line), file) != NULL) {
+        // initialize the currentId 
         int currentId = 0;
+        // scan the line and store the id in the currentId variable
         sscanf(line, "%d,", &currentId);
+        // if the currentId is equal to the studentId then set the pos variable to the current position in the file
         if (currentId == studentId) {
+            // this means that we found the student the we change pos to the current position in the file 
             pos = ftell(file) - strlen(line);
+            // then we break the loop
             break;
         }
     }
 
+    // if pos is still -1 then this means that the student is not found, so we close the file and return 0
     if (pos == -1) {
         printf("Error finding student position in the file.\n");
         fclose(file);
         return 0;
     }
 
+    // otherwise we initialize the student struct and read the student information from the file
+    // in order to get the new information from the user that needs to be modified or replaced
     Student student;
     printf("Enter modified full name (e.g., John Doe): ");
-    getchar(); // Clear the newline character from the previous input
+    getchar(); // clear the newline character from the previous input
     fgets(student.name, sizeof(student.name), stdin);
     student.name[strcspn(student.name, "\n")] = '\0'; // Remove the newline
 
@@ -196,19 +225,20 @@ int modifyStudent(const char *filename) {
     fgets(student.birthDate, sizeof(student.birthDate), stdin);
     student.birthDate[strcspn(student.birthDate, "\n")] = '\0';
 
-    // Validate the modified birth date format
+    // if the birth date format is invalid then we close the file and return 0
     if (!isValidDateFormat(student.birthDate)) {
         printf("Invalid birth date format. Please use yyyy-mm-dd.\n");
         fclose(file);
         return 0;
     }
 
+    // same process as the previous function of add student
     int year;
     sscanf(student.birthDate, "%d", &year);
 
     // Check if the birth year falls within the specified range
     if (!isValidBirthYear(year)) {
-        printf("Invalid birth year. Please enter a value between 1900 and 2023.\n");
+        printf("Invalid birth year. Please enter a value between 1995 (Start of AUI) and 2010.\n");
         fclose(file);
         return 0;
     }
@@ -232,6 +262,7 @@ int modifyStudent(const char *filename) {
     printf("Enter modified CGPA (0-4): ");
     scanf("%f", &student.cgpa);
 
+    // Check if the cgpa fall within the specified range
     if (!isValidCGPA(student.cgpa)) {
         printf("Invalid CGPA. Please enter a value between 0 and 4.\n");
         fclose(file);
@@ -241,9 +272,12 @@ int modifyStudent(const char *filename) {
     printf("Enter modified entry semester (e.g., Fall 2021): ");
     scanf(" %[^\n]", student.entrySemester);
 
+    // finaly, we use fseek to move the file pointer to the position of the student in the file
+    // and then we print the student information with the new information in the file
     fseek(file, pos, SEEK_SET);
     fprintf(file, "%d,%s,%s,%s,%s,%d,%.2f,%s\n", studentId, student.name, student.birthDate, student.school, student.major, student.credits, student.cgpa, student.entrySemester);
 
+    // close the file and return 1 for a success status
     fclose(file);
     printf("\n\n----> Student with ID %d modified successfully.\n\n", studentId);
     return 1; 
@@ -251,46 +285,58 @@ int modifyStudent(const char *filename) {
 
 ////////////////////////////////////// Delete Student //////////////////////////////////////
 int deleteStudent(const char *filename) {
+    // Open the file in read mode, because we wont delete from this file 
+    // instead we will copy the content of this file to a temporary file
+    // except the student that we want to delete
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening file.\n");
         return 0;
     }
 
+    // same as before 
     int studentId;
     printf("Enter student ID to delete: ");
     scanf("%d", &studentId);
 
+    // same as before
     if (!studentExists(studentId, filename)) {
         printf("Student with ID %d does not exist.\n", studentId);
         fclose(file);
         return 0;
     }
 
+    // here we create a temporary file to copy the content of the original file except the student that we want to delete
     FILE *tempFile = fopen("temp.txt", "w");
     if (tempFile == NULL) {
         printf("Error creating temporary file.\n");
         fclose(file);
         return 0;
     }
-
+    
+    // we initialize the line array to 100 in size
     char line[100];
+    // loop through the file line by line
     while (fgets(line, sizeof(line), file) != NULL) {
         int currentId = 0;
+        // scan the line and store the id in the currentId variable
         sscanf(line, "%d,", &currentId);
+        // as long as the currentId is not equal to the studentId then we copy the line to the temporary file
         if (currentId != studentId) {
             fputs(line, tempFile);
         }
     }
 
+    // when we finish copying the content of the original file to the temporary file we close both files
     fclose(file);
     fclose(tempFile);
 
-    // Remove the original file
+    // we remove the original file
     remove(filename);
-    // Rename the temporary file to the original filename
+    // and finally rename the temporary file to the original filename which is students.txt
     rename("temp.txt", filename);
 
+    // and then return 1 for a success status
     printf("\n\n----> Student with ID %d deleted successfully.\n\n", studentId);
     return 1;
 }
